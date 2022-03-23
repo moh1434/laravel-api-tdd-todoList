3558370320b410e492bc9d41e32b779c54aa0843
moh1434 <moh1434.ma@gmail.com>
2022-03-23 23:53:53 +0300

    init TodoList unit test

---
+++ tests/Unit/TodoListTest.php
@@ -0,0 +1,18 @@
+<?php
+// ./vendor/bin/phpunit
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class TodoListTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
