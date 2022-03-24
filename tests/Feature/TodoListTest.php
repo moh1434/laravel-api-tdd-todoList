a05d820fa8134d36309e9c5e38617c8f3f692e96
moh1434 <moh1434.ma@gmail.com>
2022-03-24 21:54:59 +0300

    get the data from the dataBase

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -2,14 +2,18 @@
 // ./vendor/bin/phpunit
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\TodoList;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TodoListTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_fetch_todo_list()
     {
+        TodoList::create(['name' => 'my list']);
 
         $response = $this->getJson(route('todo-list.index'));
 
