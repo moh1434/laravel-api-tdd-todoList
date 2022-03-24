0ef5f04a6c19825a46932b31b5edeb924e18e827
moh1434 <moh1434.ma@gmail.com>
2022-03-24 21:25:40 +0300

    create TodoListController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoListController;
 
 Route::get('todo-list', [TodoListController::class, 'index'])->name('todo-list.index'); // No
