60ff119605e8b173eb3c7f0804e884783a77f0e1
moh1434 <moh1434.ma@gmail.com>
2022-03-25 14:19:53 +0300

    use route model binding

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -13,9 +13,8 @@ class TodoListController extends Controller
         return response($lists);
     }
     // TodoList $todoList
-    public function show($id)
+    public function show(TodoList $list)
     {
-        $list = TodoList::findOrFail($id);
         return response($list);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController;
 
 Route::get('todo-list', [TodoListController::class, 'index'])->name('todo-list.index'); // No
-Route::get('todo-list/{id}', [TodoListController::class, 'show'])
+Route::get('todo-list/{list}', [TodoListController::class, 'show'])
     ->name('todo-list.show');
