0b1694607f3730c12edd19aa31db6040e424fe3b
moh1434 <moh1434.ma@gmail.com>
2022-03-25 22:49:59 +0300

    use Route::apiResource

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController;
 
-Route::get('todo-list', [TodoListController::class, 'index'])->name('todo-list.index'); // No
-Route::get('todo-list/{list}', [TodoListController::class, 'show'])
-    ->name('todo-list.show');
-Route::post('todo-list', [TodoListController::class, 'store'])
-    ->name('todo-list.store');
-Route::patch('todo-list/{list}', [TodoListController::class, 'update'])
-    ->name('todo-list.update');
-Route::delete('todo-list/{list}', [TodoListController::class, 'destroy'])
-    ->name('todo-list.destroy');
+Route::apiResource('todo-list', TodoListController::class)->parameters(['todo-list' => 'list']);
