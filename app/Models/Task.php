83ea6389a81eedfe48a93cdf02dc167a6d88e17f
moh1434 <moh1434.ma@gmail.com>
2022-03-26 23:54:47 +0300

    test_task_belongs_to_a_todo_list

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'todo_list_id'];
+
+    public function todo_list(): BelongsTo
+    {
+        return $this->belongsTo(TodoList::class);
+    }
 }
