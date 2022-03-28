7a134bef15f01af113aa65c3d94131f8cfd9626d
moh1434 <moh1434.ma@gmail.com>
2022-03-28 23:51:59 +0300

    Test login1

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TodoListController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 
 Route::apiResource('todo-list', TodoListController::class)->parameters(['todo-list' => 'list']);
@@ -15,3 +16,5 @@ Route::post('task/completed', []);
 
 
 Route::post('register', RegisterController::class)->name('user.register');
+
+Route::post('login', LoginController::class)->name('user.login');
---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_a_user_can_login_with_email_and_password()
+    {
+        $user = User::factory()->create();
+        $response = $this->postJson(route('user.login'), [
+            'email' => $user->email,
+            'password' => 'password'
+        ])->assertOk();
+
+        $this->assertArrayHasKey('token', $response->json());
+    }
+
+    public function test_if_user_email_is_not_available_then_it_return_error()
+    {
+        $response = $this->postJson(route('user.login'), [
+            'email' => 'moh1434.ma@gmail.com',
+            'password' => 'password'
+        ])->assertUnauthorized();
+    }
+
+    public function test_it_raise_error_if_password_is_incorrect()
+    {
+        $user = User::factory()->create();
+        $this->postJson(route('user.login'), [
+            'email' => $user->email,
+            'password' => 'Wrong password'
+        ])->assertUnauthorized();
+    }
+}
