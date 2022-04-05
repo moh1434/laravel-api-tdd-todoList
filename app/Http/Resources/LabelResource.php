91d47774c1fc4047e9b2aba2a9f773e34ab05111
moh1434 <moh1434.ma@gmail.com>
2022-04-05 18:08:35 +0300

    add LabelResource

--- app/Http/Resources/LabelResource.php
+++ app/Http/Resources/LabelResource.php
@@ -14,6 +14,10 @@ class LabelResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'color' => $this->color
+        ];
     }
 }
--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -15,8 +15,10 @@ class TaskResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'todo_list' => $this->todo_list->name,
+            'label' => new LabelResource($this->label),
             'created_at' => $this->created_at->diffForHumans()
         ];
     }
