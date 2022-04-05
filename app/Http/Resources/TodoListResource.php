b1e375d6601bcfbd237a7606f390ec6e6dcec4d5
moh1434 <moh1434.ma@gmail.com>
2022-04-05 18:08:03 +0300

    add TodoListRsoure content

--- app/Http/Resources/TodoListResource.php
+++ app/Http/Resources/TodoListResource.php
@@ -14,6 +14,10 @@ class TodoListResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'created_at' => $this->created_at->diffForHumans()
+        ];
     }
 }
